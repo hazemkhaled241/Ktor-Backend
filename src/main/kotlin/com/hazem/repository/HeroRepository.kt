81d4a61875dae896d30d34af13dcfb782a574b11
hazem khaled <hazemk778@gmail.com>
2023-09-05 00:39:43 +0300

    create heroes repository and it's implementation

---
+++ src/main/kotlin/com/hazem/repository/HeroRepository.kt
@@ -0,0 +1,15 @@
+package com.hazem.repository
+
+import com.hazem.models.ApiResponse
+import com.hazem.models.Hero
+
+interface HeroRepository {
+    val heroes: Map<Int, List<Hero>>
+    val page1: List<Hero>
+    val page2: List<Hero>
+    val page3: List<Hero>
+    val page4: List<Hero>
+    val page5: List<Hero>
+    suspend fun getAllHeroes(page: Int = 1): ApiResponse
+    suspend fun searchHeroes(name: String): ApiResponse
+}
